71b281feb7a928353e3f384b1b269f631832c7c3
Martin Marcher <marcher1210@gmail.com>
2013-04-20 17:57:53 +0200

    Added Wizard window and four templates for the steps in the wizard

--- LittleSmartTool2/src/littlesmarttool2/LittleSmartTool2.java
+++ LittleSmartTool2/src/littlesmarttool2/LittleSmartTool2.java
@@ -4,6 +4,9 @@
  */
 package littlesmarttool2;
 
+import javax.swing.JPanel;
+import littlesmarttool2.GUI.*;
+
 /**
  *
  * @author Rasmus
@@ -14,6 +17,6 @@ public class LittleSmartTool2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new SS2Wizard(new JPanel[]{new Step1Panel(), new Step2Panel(), new Step3Panel(), new Step4Panel()});
     }
 }
