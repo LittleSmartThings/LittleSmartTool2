9f6a6231194810cd74b33b7149c0f3689b35166c
rasmusgreve <rasmusgreve@gmail.com>
2013-04-20 17:28:05 +0200

    Added 3 types of input viewers

--- LittleSmartTool2/src/littlesmarttool2/LittleSmartTool2.java
+++ LittleSmartTool2/src/littlesmarttool2/LittleSmartTool2.java
@@ -4,6 +4,9 @@
  */
 package littlesmarttool2;
 
+import javax.swing.JFrame;
+import littlesmarttool2.GUI.TestFrame;
+
 /**
  *
  * @author Rasmus
@@ -15,5 +18,8 @@ public class LittleSmartTool2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("TODO: Write code");
+        JFrame frame = new TestFrame();
+        frame.setVisible(true);
     }
 }
