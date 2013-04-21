f4ac3c2a46907afddbf7790e3b943508a3bd0b4c
rasmusgreve <rasmusgreve@gmail.com>
2013-04-21 15:36:21 +0200

    Change in controltype on channel configurator is now updated immediately

--- LittleSmartTool2/src/littlesmarttool2/GUI/components/NWayInputViewer.java
+++ LittleSmartTool2/src/littlesmarttool2/GUI/components/NWayInputViewer.java
@@ -55,6 +55,7 @@ public class NWayInputViewer extends InputViewer {
     @Override
     public void paintComponent(Graphics g)
     {
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         
         g2.setColor(Color.white);
--- LittleSmartTool2/src/littlesmarttool2/GUI/components/StickInputViewer.java
+++ LittleSmartTool2/src/littlesmarttool2/GUI/components/StickInputViewer.java
@@ -57,6 +57,7 @@ public class StickInputViewer extends InputViewer {
     @Override
     public void paintComponent(Graphics g)
     {
+        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
         //Draw border
