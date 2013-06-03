1266576611c67e13ee9e58eaefd7c048337af9b5
rasmusgreve <rasmusgreve@gmail.com>
2013-06-03 11:53:03 +0200

    Firmware update method implemented

---
+++ LittleSmartTool2/src/littlesmarttool2/util/UpdateUtil.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package littlesmarttool2.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import littlesmarttool2.GUI.SS2Wizard;
+
+/**
+ *
+ * @author Rasmus
+ */
+public class UpdateUtil {
+    
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Updating");
+        boolean r = UpdateFirmware("COM8");
+        System.out.println("Works: " + r);
+    }
+    
+    public static boolean UpdateFirmware(String port)
+    {
+        boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+        int result;
+        String prog = (isWindows) ? "avrdude-win/avrdude.exe" : "avrdude-mac/avrdude";
+        String conf = (isWindows) ? "avrdude-win/avrdude.conf" : "avrdude-mac/avrdude.conf";
+        //String filename = "src/littlesmarttool2/firmware/Blink_10.cpp.hex";
+        String filename = "src/littlesmarttool2/firmware/StratoSnapper_v22.cpp.hex";
+        StringBuilder sb = new StringBuilder();
+        try {
+            Process p = Runtime.getRuntime().exec(prog+" -C"+conf+" -v -v -v -v -patmega328p -carduino -P"+port+" -b57600 -D -Uflash:w:"+filename+":i");
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String line;
+            while ((line = r.readLine()) != null)
+            {
+                sb.append(line);
+            }
+            result = p.waitFor();
+            
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(SS2Wizard.class.getName()).log(Level.SEVERE, sb.toString(), ex);
+            result = -1;
+        }
+        return result == 0;
+    }
+}
