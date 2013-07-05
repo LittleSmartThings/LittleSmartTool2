7967729e5db6bcb45d9f229570ecb444f45fa3d7
rasmusgreve <rasmusgreve@gmail.com>
2013-07-06 00:50:41 +0200

    Updated version number to 1.3

--- LittleSmartTool2/src/littlesmarttool2/util/UpdateUtil.java
+++ LittleSmartTool2/src/littlesmarttool2/util/UpdateUtil.java
@@ -6,7 +6,6 @@ package littlesmarttool2.util;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
